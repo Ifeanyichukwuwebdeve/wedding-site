a321d5495db7a92dd6c1bdd386e0ee6b8ecd3cc1
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2020-07-02 03:07:16 +0100

    initial commit

--- js/main.js
+++ js/main.js
@@ -42,3 +42,75 @@
 
   // Form
 
+  const form = document.querySelector('#form')
+  const name = document.querySelector('#name')
+  const email = document.querySelector('#email')
+  const subject = document.querySelector('#subject')
+  const message = document.querySelector('#message')
+  
+  form.addEventListener('submit', (e) => {
+      e.preventDefault()
+  
+      checkInputs()
+  })
+  
+  
+  
+  const checkInputs = () => {
+      // Get the value from inputs
+      const nameValue = name.value.trim()
+      const emailValue = email.value.trim()
+      const subjectValue = subject.value.trim()
+      const messageValue = message.value.trim()
+  
+      if (nameValue === '') {
+          setErrorFor(name, 'Name cannot be blank')
+      } 
+      else {
+          setSuccessFor(name, '')
+      }
+  
+      if (emailValue === '') {
+          setErrorFor(email, 'Email cannot be blank')
+      }
+      else if (!isEmail(emailValue)) {
+          setErrorFor(email, 'Email is not valid')
+      }
+      else {
+          setSuccessFor(email, ' We\'ll never share your email with anyone else.')
+      }
+  
+      if (subjectValue === '') {
+          setErrorFor(subject, 'Subject cannot be blank')
+      }else{
+          setSuccessFor(subject, '')  
+      }
+      if (messageValue === '') {
+          setErrorFor(message, 'Message check is blank')
+      }else{
+          setSuccessFor(message, '')
+      }
+  
+  }
+  
+  
+  const setErrorFor = (input, message) => {
+      const formGroup = input.parentElement
+      const small = formGroup.querySelector('small')
+  
+      small.innerText = message
+  
+      formGroup.className = 'form-group error'
+  }
+  
+  const setSuccessFor = (input, message) => {
+      const formGroup = input.parentElement
+      const small = formGroup.querySelector('small')
+  
+      small.innerText = message
+      formGroup.className = 'form-group successs'
+  }
+  
+  const isEmail = (email) => {
+      return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+  }
